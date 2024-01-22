3c6cbdfb9cdb42efc100c2d48404cdb3ffe9ef64
Leonardo Caon <leolcncaon@gmail.com>
2024-01-22 14:22:58 -0300

    config: Adicionado arquivo de environment e de configuração do aplicativo.

--- src/main/java/com/javastudies/javastudiesdemo/env/Environment.java
+++ src/main/java/com/javastudies/javastudiesdemo/env/Environment.java
@@ -1,4 +1,8 @@
 package com.javastudies.javastudiesdemo.env;
 
 public class Environment {
+    public static void main(String[] args) {
+        System.setProperty("MOCK_AUTHORIZE", "https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc");
+        System.setProperty("MOCK_NOTIFICATION", "https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6");
+    }
 }
