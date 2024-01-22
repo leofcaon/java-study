63de0a3578e2ae6782b0daf08e74c54c1dd1a5e2
Leonardo Caon <leolcncaon@gmail.com>
2024-01-22 14:23:51 -0300

    config: Adicionado classe Environment a classe Main da aplicação.

--- src/main/java/com/javastudies/javastudiesdemo/JavastudiesApplication.java
+++ src/main/java/com/javastudies/javastudiesdemo/JavastudiesApplication.java
@@ -1,13 +1,15 @@
 package com.javastudies.javastudiesdemo;
 
+import com.javastudies.javastudiesdemo.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JavastudiesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavastudiesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Environment.main(args);
+        SpringApplication.run(JavastudiesApplication.class, args);
+    }
 
 }
